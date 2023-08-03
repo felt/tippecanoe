4ebdd6d9e6ba3fd94397c7402225804bd5b76ddf
Erica Fischer <enf@felt.com>
2023-08-03 12:07:25 -0700

    Change overzoom to accept input and output files as arguments

--- overzoom.cpp
+++ overzoom.cpp
@@ -134,18 +134,23 @@ std::string overzoom(std::string s, int oz, int ox, int oy, int nz, int nx, int 
 }
 
 void usage(char **argv) {
-	fprintf(stderr, "Usage: %s oz ox oy nz nx ny < tile.pbf.gz > newtile.pbf.gz\n", argv[0]);
+	fprintf(stderr, "Usage: %s -o newtile.pbf.gz tile.pbf.gz oz/ox/oy nz/nx/ny\n", argv[0]);
 	fprintf(stderr, "to create tile nz/nx/ny from tile oz/ox/oy\n");
 	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv) {
 	int i;
+	const char *outfile = NULL;
 
-	while ((i = getopt(argc, argv, "y:")) != -1) {
+	while ((i = getopt(argc, argv, "y:o:")) != -1) {
 		switch (i) {
 		case 'y':
 			keep.insert(optarg);
+			break;
+
+		case 'o':
+			outfile = optarg;
 			break;
 
 		default:
@@ -153,27 +158,52 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	if (argc - optind != 6) {
+	if (argc - optind != 3) {
 		usage(argv);
 	}
 
-	int oz = atoi(argv[optind]);
-	int ox = atoi(argv[optind + 1]);
-	int oy = atoi(argv[optind + 2]);
+	if (outfile == NULL) {
+		usage(argv);
+	}
 
-	int nz = atoi(argv[optind + 3]);
-	int nx = atoi(argv[optind + 4]);
-	int ny = atoi(argv[optind + 5]);
+	const char *infile = argv[optind + 0];
+
+	int oz, ox, oy;
+	if (sscanf(argv[optind + 1], "%d/%d/%d", &oz, &ox, &oy) != 3) {
+		fprintf(stderr, "%s: not in z/x/y form\n", argv[optind + 1]);
+		usage(argv);
+	}
+
+	int nz, nx, ny;
+	if (sscanf(argv[optind + 1], "%d/%d/%d", &nz, &nx, &ny) != 3) {
+		fprintf(stderr, "%s: not in z/x/y form\n", argv[optind + 1]);
+		usage(argv);
+	}
 
 	std::string tile;
 	char buf[1000];
 	int len;
 
-	while ((len = fread(buf, sizeof(char), 1000, stdin)) > 0) {
+	FILE *f = fopen(infile, "rb");
+	if (f == NULL) {
+		perror(infile);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((len = fread(buf, sizeof(char), 1000, f)) > 0) {
 		tile.append(std::string(buf, len));
+	}
+	fclose(f);
+
+	f = fopen(outfile, "wb");
+	if (f == NULL) {
+		perror(outfile);
+		exit(EXIT_FAILURE);
 	}
 
 	std::string out = overzoom(tile, oz, ox, oy, nz, nx, ny);
-	fwrite(out.c_str(), sizeof(char), out.size(), stdout);
+	fwrite(out.c_str(), sizeof(char), out.size(), f);
+	fclose(f);
+
 	return 0;
 }
