6f8273a4c808c292e939b269e0116a45004ab553
Erica Fischer <enf@felt.com>
2024-10-09 12:27:33 -0700

    The multiplier cap was a bad idea

--- tile.cpp
+++ tile.cpp
@@ -1237,9 +1237,8 @@ static serial_feature next_feature(decompressor *geoms, std::atomic<long long> *
 			} else if (z + extra_multiplier_zooms >= feature_minzoom && count->second + 1 < retain_points_multiplier) {
 				count->second++;
 				sf.dropped = count->second;
-			} else if (preserve_multiplier_density_threshold > 0 && count->second + 1 < 2 * retain_points_multiplier &&
+			} else if (preserve_multiplier_density_threshold > 0 &&
 				   sf.gap > ((1LL << (32 - z)) / preserve_multiplier_density_threshold) * ((1LL << (32 - z)) / preserve_multiplier_density_threshold)) {
-				count->second++;
 				sf.dropped = FEATURE_ADDED_FOR_MULTIPLIER_DENSITY;
 			} else {
 				sf.dropped = FEATURE_DROPPED;
